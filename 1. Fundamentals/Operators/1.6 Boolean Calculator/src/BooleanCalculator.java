4ff7e586048adbf0d5df47b3743957050a6a058c
Juan <juanlopezdehierro@gmail.com>
2023-09-21 10:26:24 +0100

    Second commit

--- 1. Fundamentals/Operators/1.6 Boolean Calculator/src/BooleanCalculator.java
+++ 1. Fundamentals/Operators/1.6 Boolean Calculator/src/BooleanCalculator.java
@@ -4,5 +4,26 @@ public class BooleanCalculator
    {
       int a = 3, b = -4, c = 1;
       boolean t = true, f = false;
+      if (a >= b){
+         System.out.println("TRUE");
+      }else{
+         System.out.println("FALSE");
+      }if (-b/a == c){
+         System.out.println("TRUE");
+      }else{
+         System.out.println("FALSE");
+      }
+      boolean b2 = 2 * c <= c;
+      if ((t || !f) == b2) {
+         System.out.println("TRUE");
+      }else{
+         System.out.println("FALSE");
+      }
+      boolean b1 = b + c / a > a - b * c;
+      if ((!t == f) != b1) {
+         System.out.println("TRUE");
+      }else{
+         System.out.println("FALSE");
+      }
    }
 }
