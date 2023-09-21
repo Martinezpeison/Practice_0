c885cff3e438a0c7d88ee614f12ffb991662d761
Juan <juanlopezdehierro@gmail.com>
2023-09-21 11:12:33 +0100

    Final commit

--- 2. Flow Control/Loops/2.8 Hundredth Prime/src/HundredthPrime.java
+++ 2. Flow Control/Loops/2.8 Hundredth Prime/src/HundredthPrime.java
@@ -1,7 +1,32 @@
-public class HundredthPrime
-{
-   public static void main(String[] args)
-   {
-      /* Make a code that displays the 100th prime number */
+public class HundredthPrime {
+   public static void main(String[] args) {
+      int n = 100; // El número primo que quieres encontrar (en este caso, el centésimo).
+
+      int count = 0; // Contador para llevar la cuenta de los números primos encontrados.
+      int num = 2; // El número actual que estamos comprobando.
+
+      while (count < n) {
+         if (isPrime(num)) {
+            count++;
+            if (count == n) {
+               System.out.println(num);
+               break; // Hemos encontrado el centésimo número primo, salimos del bucle.
+            }
+         }
+         num++;
+      }
+   }
+
+   // Método para comprobar si un número es primo.
+   public static boolean isPrime(int num) {
+      if (num <= 1) {
+         return false;
+      }
+      for (int i = 2; i <= Math.sqrt(num); i++) {
+         if (num % i == 0) {
+            return false;
+         }
+      }
+      return true;
    }
 }
